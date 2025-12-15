27a59754ce498f3f940d184718fe460968b69de5
ddrapei <d.drapei@rgu.ac.uk>
2025-12-15 16:04:58 +0000

    parsing a number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter 2 numbers you need to find greatest common diviser for:");
+
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("Failed to read the line.");
+
+    let parsed_number = user_input.trim().parse::<i32>();
+
+    let number = match parsed_number {
+        Ok(num)=>num,
+        Err(_) => {
+            println!("That input can't be parsed, enter a number");
+            0
+        }
+    };
+    
+    println!("User input: {}", number);    
 }
